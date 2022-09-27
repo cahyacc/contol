373e63249b16b18b7da09fa47f54bd58d09f6e50
ZassXdOfficial <98353608+ZassXdOfficial@users.noreply.github.com>
2022-09-28 00:18:37 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,161 @@
+exports.allmenu = (prefix) => {
+return `
+─[ *𝑀𝒜𝐼𝒩 𝑀𝐸𝒩𝒰* ]
+➾ *•* ${prefix}db
+➾ *•* ${prefix}tes
+➾ *•* ${prefix}dev
+➾ *•* ${prefix}owner
+➾ *•* ${prefix}runtime
+➾ *•* ${prefix}dashboard
+
+
+[ *𝒪𝒯𝐻𝐸𝑅 𝑀𝐸𝒩𝒰* ]
+➾ *•* ${prefix}ping
+➾ *•* ${prefix}simi
+➾ *•* ${prefix}rules
+➾ *•* ${prefix}listpc
+➾ *•* ${prefix}listgc
+➾ *•* ${prefix}groupwa
+
+
+[ *𝐹𝒰𝒩 𝑀𝐸𝒩𝒰* ]
+➾ *•* ${prefix}jadian
+➾ *•* ${prefix}jodohku
+➾ *•* ${prefix}confes
+➾ *•* ${prefix}menfes
+➾ *•* ${prefix}rate
+➾ *•* ${prefix}apakah
+➾ *•* ${prefix}bisakah
+➾ *•* ${prefix}siapakah
+➾ *•* ${prefix}bagaimanakah
+
+
+[ *𝒯𝒪𝒪𝐿𝒮 𝑀𝐸𝒩𝒰* ]
+➾ *•* ${prefix}tts
+➾ *•* ${prefix}isgd
+➾ *•* ${prefix}cuttly
+➾ *•* ${prefix}tinyurl
+➾ *•* ${prefix}nulis
+➾ *•* ${prefix}toimg
+➾ *•* ${prefix}obfus
+➾ *•* ${prefix}ssweb
+➾ *•* ${prefix}sticker
+➾ *•* ${prefix}styletext
+➾ *•* ${prefix}translate
+➾ *•* ${prefix}kalkulator
+
+
+[ *𝐵𝒜𝐼𝐿𝐸𝒴𝒮 𝑀𝐸𝒩𝒰* ]
+➾ *•* ${prefix}afk
+➾ *•* ${prefix}react
+➾ *•* ${prefix}fitnah
+➾ *•* ${prefix}hidetag
+➾ *•* ${prefix}towame
+
+
+[ *𝒮𝐸𝒯 𝒫𝑅𝒪𝒮𝐸𝒮/𝒟𝒪𝒩𝐸* ]
+➾ *•* ${prefix}setdone
+➾ *•* ${prefix}delsetdone
+➾ *•* ${prefix}changedone
+➾ *•* ${prefix}setproses
+➾ *•* ${prefix}delsetproses
+➾ *•* ${prefix}changeproses
+
+
+[ *𝒮𝐸𝒯 𝒲𝐸𝐿𝒞𝒪𝑀𝐸/𝐿𝐸𝐹𝒯* ]
+➾ *•* ${prefix}setleft
+➾ *•* ${prefix}delleft
+➾ *•* ${prefix}getleft
+➾ *•* ${prefix}changeleft
+➾ *•* ${prefix}setwelcome
+➾ *•* ${prefix}delwelcome
+➾ *•* ${prefix}getwelcome
+➾ *•* ${prefix}changewelcome
+
+
+[ *𝒢𝑅𝒪𝒰𝒫 𝑀𝐸𝒩𝒰* ]
+➾ *•* ${prefix}left
+➾ *•* ${prefix}add
+➾ *•* ${prefix}kick
+➾ *•* ${prefix}open
+➾ *•* ${prefix}close
+➾ *•* ${prefix}tagall
+➾ *•* ${prefix}delete
+➾ *•* ${prefix}revoke
+➾ *•* ${prefix}antilink
+➾ *•* ${prefix}welcome
+➾ *•* ${prefix}hidetag
+➾ *•* ${prefix}demote
+➾ *•* ${prefix}setdesc
+➾ *•* ${prefix}linkgrup
+➾ *•* ${prefix}promote
+➾ *•* ${prefix}setppgrup
+➾ *•* ${prefix}setnamegc
+
+
+[ *𝒪𝒲𝒩𝐸𝑅 𝑀𝐸𝒩𝒰* ]
+➾ *•* ${prefix}bc
+➾ *•* ${prefix}join
+➾ *•* ${prefix}leave
+➾ *•* ${prefix}block
+➾ *•* ${prefix}resetdb
+➾ *•* ${prefix}unblock
+➾ *•* ${prefix}creategc
+➾ *•* ${prefix}sendsesi
+➾ *•* ${prefix}setppbot
+➾ *•* ${prefix}setbiobot
+➾ *•* ${prefix}broadcast
+
+
+[ *𝒮𝒯𝒜𝐿𝒦𝐸𝑅 𝑀𝐸𝒩𝒰* ]
+➾ *•* ${prefix}igstalk
+➾ *•* ${prefix}ffstalk
+➾ *•* ${prefix}mlstalk
+➾ *•* ${prefix}npmstalk
+➾ *•* ${prefix}githubstalk
+
+
+[ *𝒱𝒪𝒦𝒜𝐿 𝑀𝐸𝒩𝒰* ]
+➾ *•* ${prefix}hilih
+➾ *•* ${prefix}huluh
+➾ *•* ${prefix}heleh
+➾ *•* ${prefix}holoh
+➾ *•* ${prefix}halah
+
+
+[ *𝐼𝒩𝐹𝒪𝑅𝑀𝒜𝒯𝐼𝒪𝒩* ]
+➾ *•* ${prefix}cuaca
+➾ *•* ${prefix}kodepos
+➾ *•* ${prefix}infogempa
+➾ *•* ${prefix}covidworld
+
+
+[ *𝒟𝒪𝒲𝒩𝐿𝒪𝒜𝒟𝐸𝑅* ]
+➾ *•* ${prefix}play
+➾ *•* ${prefix}tiktok
+➾ *•* ${prefix}ytmp3
+➾ *•* ${prefix}ytmp4
+➾ *•* ${prefix}gitclone
+➾ *•* ${prefix}mediafire
+➾ *•* ${prefix}tiktokmp3
+➾ *•* ${prefix}tiktoknowm
+
+
+[ *𝒟𝐸𝒱𝐸𝐿𝒪𝒫𝐸𝑅* ]
+➾ *•* ${prefix}jadibot [ only ]
+➾ *•* ${prefix}bugfc [ options ] 
+➾ *•* ${prefix}buggc [ options ]
+➾ *•* ${prefix}spam [ reply bug ]
+➾ *•* ${prefix}senbug [ bug pilihan ]
+➾ *•* ${prefix}bugvip [ bug pilihan ]
+➾ *•* ${prefix}bugpoll [ bug vote ]
+➾ *•* ${prefix}bugtag [ bug tagall ]
+➾ *•* ${prefix}catalog [ bug catalog ]
+➾ *•* ${prefix}docu [ bug document ]
+➾ *•* ${prefix}daca [ bug document ]
+➾ *•* ${prefix}troli [ bug troli ]
+➾ *•* ${prefix}troli2 [ bug troli 2 ]
+➾ *•* ${prefix}bugstik [ bug stiker ]
+➾ *•* ${prefix}buglokasi [ bug lokasi ]
+`
+}
